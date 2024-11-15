1452229f13a7ac93cee8327c7755499b8644fe97
Mo7TaRiF <eng.mo7tarif@gmail.com>
2024-11-15 15:23:35 +0200

    added more games by date, 15/11/2024

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def add_game(game):
     with open("Highlight Rows with Recent Dates and Specific Games.user.js", encoding="UTF-8") as buy_order_script:
         script = buy_order_script.read()
         current_version = script.split()[17]
-        new_version = float(current_version) + 0.01
+        new_version = round(float(current_version) + 0.01, 2)
 
         script = script.replace(f"// @version      {current_version}",f"// @version      {new_version}")
         script =  script.replace(f''',
